112023cb77ed2b1f166ba3d4a4a4a775254c4bec
Jason Tee <teej@bc.edu>
2019-08-12 15:48:58 -0400

    Create protected route

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -4,7 +4,19 @@ import { Route, Redirect, withRouter } from 'react-router-dom';
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => {
     return (
+        <Route path={path} exact={exact} render={(props) => (
+            !loggedIn ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect to="/" />
+                )
+        )} />
+    )
 
+};
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => {
+    return (
         <Route path={path} exact={exact} render={(props) => (
             loggedIn ? (
                 <Component {...props} />
@@ -14,11 +26,15 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => {
         )} />
     )
 
-}
-;
+};
 
 const mapStateToProps = state => {
     return { loggedIn: Boolean(state.session.id) };
 };
 
+const msp = state => {
+    return { loggedin: Boolean(state.session.id)}
+}
+
 export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(msp, null)(Protected));
