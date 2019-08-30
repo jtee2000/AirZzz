a235534ec3d051f7408fbd9d008619952969e76e
Jason Tee <teej@bc.edu>
2019-08-30 17:09:40 -0400

    Add beds to listing index

--- frontend/components/listings/listing_index_item.jsx
+++ frontend/components/listings/listing_index_item.jsx
@@ -28,7 +28,7 @@ class ListingIndexItem extends React.Component {
                     <Link to={`/listings/show/${this.props.listing.id}`}>
                         <img className="listing-main-img" src={this.props.listing.photoUrl[0].service_url}/>
                         <div className="listing-details-container">
-                            <p className="list-loc-ele">{this.props.listing.title.toUpperCase()}</p>
+                            <p className="list-loc-ele">{this.props.listing.title.toUpperCase()} • {this.props.listing.beds} beds</p>
                             <p className="list-desc-ele">Boston College</p>
                             <p className="list-price-ele">${this.props.listing.price}/night</p>
                             {/* <span><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><i className="fas fa-star"></i><span className="star-reviews"> No Reviews</span></span> */}
