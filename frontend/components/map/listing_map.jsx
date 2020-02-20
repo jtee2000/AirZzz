aa5ceaf47faaf8c86e3c51d7937b3e8cc8c863c7
Jason Tee <teej@bc.edu>
2020-02-20 16:53:29 -0500

    Add bookings and delete

--- frontend/components/map/listing_map.jsx
+++ frontend/components/map/listing_map.jsx
@@ -9,25 +9,18 @@ class ListingMap extends React.Component {
         this.registerListeners = this.registerListeners.bind(this);
         this.makeMapWork = this.makeMapWork.bind(this);
     }
-
-
     componentDidMount() {
         const mapOptions = {
             center: { lat: 42.335814, lng: -71.168923 }, 
             zoom: 13
         };
-        // wrap this.mapNode in a Google Map
         this.map = new google.maps.Map(this.mapNode, mapOptions);
-        // this.map = new google.maps.Map(mapDOMNode, mapOptions);
         this.MarkerManager = new MarkerManager(this.map);
-        // this.props.fetchListings().then( (payload) => this.MarkerManager.updateMarkers(payload)); 
         this.registerListeners();
         this.MarkerManager.updateMarkers(this.props.listings);
-        // Integrate Google Places Search Feature 
         google.maps.event.addListener(this.map, 'bounds_changed', () => {
             var input = document.getElementById('airzzz-search');
             this.autocomplete = new google.maps.places.Autocomplete(input);
-            // this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
             this.autocomplete.setBounds(this.map.getBounds());
             this.makeMapWork();
         });
@@ -37,8 +30,6 @@ class ListingMap extends React.Component {
     makeMapWork() {
         
         var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
         const map = this.map;
         google.maps.event.addListener(this.autocomplete, 'place_changed', () => {
             var place = this.autocomplete.getPlace();
@@ -55,58 +46,12 @@ class ListingMap extends React.Component {
                     bounds.extend(place.geometry.location);
                 }
             map.fitBounds(bounds);
-            // this.props.history.push("/map");
-            // Clear out the old markers.
-            // markers.forEach(function (marker) {
-                //     marker.setMap(null);
-            // });
-            // markers = [];
-            // // For each place, get the icon, name and location.
-            // var bounds = new google.maps.LatLngBounds();
-            // places.forEach(function (place) {
-            //     if (!place.geometry) {
-            //         console.log("Returned place contains no geometry");
-            //         return;
-            //     }
-            //     var icon = {
-            //         url: place.icon,
-            //         size: new google.maps.Size(71, 71),
-            //         origin: new google.maps.Point(0, 0),
-            //         anchor: new google.maps.Point(17, 34),
-            //         scaledSize: new google.maps.Size(25, 25)
-            //     };
-            //     // Create a marker for each place.
-            //     markers.push(new google.maps.Marker({
-            //         map: map,
-            //         icon: icon,
-            //         title: place.name,
-            //         position: place.geometry.location
-            //     }));
-            //     if (place.geometry.viewport) {
-            //         // Only geocodes have viewport.
-            //         bounds.union(place.geometry.viewport);
-            //     } else {
-            //         bounds.extend(place.geometry.location);
-            //     }
-            // });
-            // 
         });
     }
 
     componentDidUpdate() {
         this.MarkerManager.updateMarkers(this.props.listings);
-        // this.makeMapWork();
     }
-
-    // componentDidUpdate() {
-    //     if (this.props.singleBench) {
-    //         const targetListingKey = Object.keys(this.props.listings)[0];
-    //         const targetListing = Object.values(this.props.listings)[targetListingKey];
-    //         this.MarkerManager.updateMarkers([targetListing]); //grabs only that one bench
-    //     } else {
-    //         this.MarkerManager.updateMarkers(this.props.listings);
-    //     }
-    // }
 
     registerListeners() {
         google.maps.event.addListener(this.map, 'idle', () => {
